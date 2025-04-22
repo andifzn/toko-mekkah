a61f6f8989d92d2d036ee858db0b639fe1439c60
Andi Fauzan <andifauzan586@gmail.com>
2025-04-22 11:53:54 +0800

    .;

---
+++ script.js
@@ -0,0 +1,20 @@
+const navbarMenu = document.querySelector('.navbar-menu');
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+// Hamburger Menu Section
+// Open Navbar menu
+hamburgerMenu.addEventListener('click', (e) => {
+    e.stopPropagation();
+    navbarMenu.classList.toggle('active')
+});
+
+// Close Navbar Menu
+document.addEventListener('click', (e) => {
+    // Logikanya 
+    // jika didalam navbarMenu mengandung class active dan yang di klik bukan di dalam area navbarMenu
+    if (navbarMenu.classList.contains('active') && !navbarMenu.contains(e.target)) {
+        navbarMenu.classList.remove('active')
+    }
+});
+
